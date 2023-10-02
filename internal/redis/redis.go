f5152073158da317579a8a5ed723055e73698318
VKOVA <vlad13102002@yandex.ru>
2023-10-02 22:20:31 +0300

    add parameters for config redis

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -10,12 +10,12 @@ type Client struct {
 	client *redis.Client
 }
 
-func New(ctx context.Context) (*Client, error) {
+func New(ctx context.Context, addr, pass string, db int) (*Client, error) {
 	redisCl := &Client{
 		client: redis.NewClient(&redis.Options{
-			Addr:     "127.0.0.1:6379",
-			Password: "password",
-			DB:       0,
+			Addr:     addr,
+			Password: pass,
+			DB:       db,
 		}),
 	}
 	if _, err := redisCl.client.Ping(ctx).Result(); err != nil {
